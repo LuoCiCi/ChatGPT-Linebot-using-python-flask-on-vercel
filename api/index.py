945f543d33782556f646f665d65ba9e02dc1706e
LuoCiCi <125720586+LuoCiCi@users.noreply.github.com>
2024-09-24 00:13:46 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -32,6 +32,36 @@ def callback():
         abort(400)
     return 'OK'
 
+# 天氣圖片抓取函式（不存圖片，只回傳圖片 URL）
+def get_weather_image_urls():
+    # 使用 ChromeDriverManager 自動管理 ChromeDriver
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service)
+
+    # 打開目標網頁
+    driver.get("https://www.cwa.gov.tw/V8/C/P/Rainfall/Rainfall_QZJ.html")
+    
+
+    # 等待網頁完全加載
+    time.sleep(5)
+
+    # 查找所有圖片元素
+    images = driver.find_elements(By.TAG_NAME, 'img')
+    image_urls = []
+
+    # 遍歷所有找到的圖片，並篩選來自 Data/rainfall 目錄的圖片
+    for img in images:
+        img_url = img.get_attribute('src')
+
+        # 只回傳來自 Data/rainfall 的圖片 URL
+        if "Data/rainfall" in img_url:
+            image_urls.append(img_url)
+
+    # 關閉瀏覽器
+    driver.quit()
+
+    return image_urls
+
 
 @line_handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
@@ -44,6 +74,7 @@ def handle_message(event):
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text="尋找天氣中"))
+        
         return
         
     if event.message.text == "說話":
