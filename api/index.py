b4210f76fc68bc8822ba4375ca5635e9d44a135a
LuoCiCi <125720586+LuoCiCi@users.noreply.github.com>
2024-09-23 19:46:58 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -53,7 +53,7 @@ def handle_message(event):
 
         line_bot_api.reply_message(
                 event.reply_token,
-                TextSendMessage(text="img"&image_urls)
+                TextSendMessage(text=image_urls)
             )
         
         if image_urls:
