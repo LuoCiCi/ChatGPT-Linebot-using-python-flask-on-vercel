c9f6fe48519a79306bd1cfc6d3aa1f74ddcd446c
LuoCiCi <125720586+LuoCiCi@users.noreply.github.com>
2024-09-23 19:43:30 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,4 +1,7 @@
-
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
