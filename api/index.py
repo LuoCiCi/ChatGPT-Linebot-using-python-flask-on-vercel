ca5f83f4c72abc0d369ce953397e51ea73ce0d5d
Allen <allen@hacontrols.com.tw>
2024-09-25 12:03:48 +0800

    測試完成  拿掉測試

--- api/index.py
+++ api/index.py
@@ -187,7 +187,6 @@ def handle_message(event):
         line_bot_api.reply_message(
             event.reply_token,
             [
-                TextSendMessage(text="圖片測試節點"),
                 ImageSendMessage(original_content_url=random_image_url, preview_image_url=random_image_url)
             ]
         )
