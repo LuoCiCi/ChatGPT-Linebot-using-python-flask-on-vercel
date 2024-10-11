47824c2df6184587b347cf1334c1168c478d922c
LuoCiCi <125720586+LuoCiCi@users.noreply.github.com>
2024-10-11 23:00:53 +0800

    Update bigbusted

--- api/index.py
+++ api/index.py
@@ -942,83 +942,85 @@ def handle_message(event):
         max_attempts = 5  # 設定最多嘗試的次數
         attempts = 0
         
-        image_urls = [
-            "https://p9.itc.cn/q_70/images03/20230904/f4fe0b05119b4ab8b0075b4c41c46ce7.jpeg",
-            "https://media.nownews.com/nn_media/thumbnail/2022/04/1649131065438-58adaef36d3d455096b168c087908bf5-800x534.jpg?unShow=false",
-            "https://s.yimg.com/zp/MerchandiseImages/28DA6596E9-SP-7775353.jpg",
-            "https://dynamic.zacdn.com/Xgdy0k_T5AnP6XBxcbfeH2V5Cmo=/filters:quality(70):format(webp)/https://static-tw.zacdn.com/p/lycka-9583-0408471-2.jpg",
-            "https://img.cloudimg.in/uploads/shops/7094/products/b0/b02c2e7c86d787f1138c05f32a7e14c7.jpg",
-            "https://s.yimg.com/zp/MerchandiseImages/983BEFE4F1-SP-7781092.jpg",
-            "https://img.alicdn.com/imgextra/i3/190954632/O1CN01vxrocX1k5VMB7ranX_!!190954632.jpg",
-            "https://gw.alicdn.com/imgextra/i3/2532877733/O1CN01xz4xuX26zlbYAJj0S_!!0-item_pic.jpg_Q75.jpg_.webp",
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgHMKS3ryrpSrZvnHWfpiyBEmnF1Oy1Tw3IQ&s",
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGchDhH0FLs53qrVu_VDFAQ80tM-H_Bq2EBg&s",
-            "https://media.zenfs.com/en/nownews.com/b34d3759f2b1382430bfa18cac22ced9",
-            "https://dailyview.tw/_next/image?url=https%3A%2F%2Fdvblobcdnjp.azureedge.net%2FContent%2FUpload%2FPopular%2FImages%2F2019-02%2F9265b389-0aca-4318-9f2f-226397be36e9_m.jpg&w=1920&q=75",
-            "https://img.ltn.com.tw/Upload/ent/page/800/2023/10/27/phpa06t7k.jpg",
-            "https://attach.setn.com/newsimages/2020/09/04/2755061-PH.jpg",
-            "https://cbu01.alicdn.com/img/ibank/O1CN01TEZ0qm1pCtlUIyYSx_!!3838865325-0-cib.jpg",
-            "https://images.chinatimes.com/newsphoto/2019-12-20/656/20191220000037.jpg",
-            "https://i.imgur.com/iq67SPm.jpg",
-            "https://cdn.hk01.com/di/media/images/dw/20210707/489482591276109824637408.jpeg/sOkuWkg6jdkim0jZYtkbEChKJv3A3Kpw3Pjr2dz469k?v=w1920",
-            "https://tshop.r10s.com/b27/50c/cfec/baef/3073/d808/4e76/1145ed930c0242ac110005.jpg?_ex=486x486",
-            "https://media.nownews.com/nn_media/thumbnail/2022/03/1647937976105-2ed3e95d6cad43238ed07dcc941aff2b-500x625.jpg?unShow=false&waterMark=false",
-            "https://s.yimg.com/zp/images/9DA66C9F780B61728BCB58F044CF4AF6F6ABBCA4",
-            "https://img.cdn.91app.hk/webapi/imagesV3/Cropped/SalePage/361662/0/638616321323030000?v=1",
-            "https://diz36nn4q02zr.cloudfront.net/webapi/imagesV3/Cropped/SalePage/8085430/0/638580602488200000?v=1",
-            "https://tshop.r10s.com/627/448/7fce/3c59/4041/e841/dfd1/116fed83c30242ac110003.jpg?_ex=486x486",
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTp3dixUyncHWWwFb93xc3ap0AMfwFEdxuLYQ&s",
-            "https://down-tw.img.susercontent.com/file/222b1cddcc028fca6be43a23c469121d",
-            "https://tshop.r10s.com/9d1/dea/5403/d6d2/308b/3e64/7ca6/11f5eeb5570242ac110002.jpg?_ex=486x486",
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOu8MbM1UM9f2qoBFArh01bLef3L2AcAbhVw&s",
-            "https://img.ltn.com.tw/Upload/ent/page/800/2021/04/22/phpqElaYW.jpg",
-            "https://images.chinatimes.com/newsphoto/2018-06-23/656/20180623002206.jpg",
-            "https://im2.book.com.tw/image/getImage?i=https://www.books.com.tw/img/001/081/57/0010815753.jpg&v=5c7666aek&w=375&h=375",
-            "https://img.ltn.com.tw/Upload/ent/page/800/2020/03/25/phpFHoymZ.jpg",
-            "https://cdn.hk01.com/di/media/images/dw/20240531/873078137703895040261407.jpeg/NNjtWk7r1hvScy8dyFMWXQJIMf8k79cvIsXtPiLF7T4?v=w1920",
-            "https://bee-men.com/uploads/ckeditor/pictures/25032/content_wendy__624_209358671_322462809344373_4100224133141901654_n.jpg",
-            "https://fs2.my-bras.com/upload/ftp/00_Product/22037000W-207-1.jpg",
-            "https://image.hkhl.hk/f/1024p0/0x0/100/none/ce50202910d349d7f7ce14913be6456f/2023-06/_alyciac_343289471_538879431656306_1742365294561665946_n.jpg",
-            "https://images.chinatimes.com/newsphoto/2020-07-08/656/20200708005597.jpg",
-            "https://thumbs.dreamstime.com/z/%E6%BC%82%E4%BA%AE%E7%9A%84%E8%82%96%E5%83%8F%EF%BC%8C%E6%80%A7%E6%84%9F%E7%9A%84%E4%BA%9A%E6%B4%B2%E5%A5%B3%E4%BA%BA%EF%BC%8C%E7%9C%8B%E8%B5%B7%E6%9D%A5%E6%80%A7%E6%84%9F%E8%BF%B7%E4%BA%BA-245021896.jpg",
-            "https://diz36nn4q02zr.cloudfront.net/webapi/imagesV3/Original/SalePage/9501574/0/638503534754030000?v=1",
-            "https://hk.on.cc/cnt/entertainment/20230625/photo/bkn-20230625210127720-0625_00862_001_02p.jpg?20230625210127",
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzolZ_BFSbZdm7m-CN2Ij8KikM4Rt7M9hafQ&s",
-            "https://images.chinatimes.com/newsphoto/2023-09-15/1024/20230915003039_20230915144323.jpg",
-            "https://s.yimg.com/ny/api/res/1.2/Ah4NjV.AOKa.b3tVnQ5Mvg--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MA--/https://media.zenfs.com/zh-tw/nownews.hk/61f741eb6c0da77dcd1b78729907cf3f",
-            "https://www.cangai.tw/wp-content/uploads/2021/06/01-3.jpg",
-            "https://www.etonwedding.com/pre-wedding-photo/img/%E5%80%8B%E4%BA%BA%E5%AF%AB%E7%9C%9F_%E9%96%A8%E8%9C%9C%E5%AF%AB%E7%9C%9F/6-%E5%80%8B%E4%BA%BA%E5%AF%AB%E7%9C%9F_%E9%96%A8%E8%9C%9C%E5%AF%AB%E7%9C%9F%20(6).jpg",
-            "https://s.yimg.com/ny/api/res/1.2/qJHMvzmdgYYx5hIypNXnsw--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MDtoPTgwMA--/https://media.zenfs.com/en/ftvn.com.tw/e5132eb34101fffdae201a88d6c30f49",
-            "https://today-obs.line-scdn.net/0hyK1CZDb3JmQMOjIUBE9ZMzRsKhU_XDxtLlxpASg4KAcpFjIwNFp1BykycEhxAjU3LFs9VX1ofV0gX2lgNA/w644",
-            "https://img.ltn.com.tw/Upload/news/600/2019/05/29/phpb85WtL.jpg",
-            "https://atctwn.com/wp-content/uploads/2021/07/main-4.jpg",
-            "https://atctwn.com/wp-content/uploads/2021/07/WNM.jpg",
-            "https://cdn2.ettoday.net/images/7803/d7803868.jpg",
-            "https://cdn2.ettoday.net/images/4243/d4243363.jpg",
-            "https://i1.kknews.cc/OZLWpFd7Sdd6e5Cj0RPSHbH-oLsCiEs_iyR54Sk/0.jpg",
-            "https://www.etonwedding.com/img/photo/personal/music/personal%20(1).jpg",
-            "https://i.getjetso.com/month_1612/20161209_28719cd2b8a32cd51239FeUOK7.jpg?name=%E6%96%B9%E7%B7%A9%E5%AF%AB%E7%9C%9F",
-            "https://cdn2.ettoday.net/images/6862/d6862321.jpg",
-            "https://static.wixstatic.com/media/931713_32eb7c8072ef49fb9628c18caeb9e143~mv2.jpg/v1/fill/w_250,h_375,al_c,q_90,enc_auto/931713_32eb7c8072ef49fb9628c18caeb9e143~mv2.jpg",
-            "https://img.ltn.com.tw/Upload/ent/page/800/2024/07/08/phpk9uyxg.jpg",
-            "https://atctwn.com/wp-content/uploads/2020/02/yoda-1.jpg",
-            "https://static.wixstatic.com/media/931713_d0f31e419e9f4099b00ac74dbc9190f6~mv2.jpg/v1/fill/w_250,h_375,al_c,q_90,enc_auto/931713_d0f31e419e9f4099b00ac74dbc9190f6~mv2.jpg",
-            "https://i2.kknews.cc/tNTbLd-nwHY_0AvlNEFZWF-wVJJztVi9Ap6WzLg/0.jpg",
-            "https://i.pinimg.com/1200x/96/c6/23/96c623821b53e8eb00e12ddabef911dd.jpg"
-        ]
+        # image_urls = [
+        #     "https://p9.itc.cn/q_70/images03/20230904/f4fe0b05119b4ab8b0075b4c41c46ce7.jpeg",
+        #     "https://media.nownews.com/nn_media/thumbnail/2022/04/1649131065438-58adaef36d3d455096b168c087908bf5-800x534.jpg?unShow=false",
+        #     "https://s.yimg.com/zp/MerchandiseImages/28DA6596E9-SP-7775353.jpg",
+        #     "https://dynamic.zacdn.com/Xgdy0k_T5AnP6XBxcbfeH2V5Cmo=/filters:quality(70):format(webp)/https://static-tw.zacdn.com/p/lycka-9583-0408471-2.jpg",
+        #     "https://img.cloudimg.in/uploads/shops/7094/products/b0/b02c2e7c86d787f1138c05f32a7e14c7.jpg",
+        #     "https://s.yimg.com/zp/MerchandiseImages/983BEFE4F1-SP-7781092.jpg",
+        #     "https://img.alicdn.com/imgextra/i3/190954632/O1CN01vxrocX1k5VMB7ranX_!!190954632.jpg",
+        #     "https://gw.alicdn.com/imgextra/i3/2532877733/O1CN01xz4xuX26zlbYAJj0S_!!0-item_pic.jpg_Q75.jpg_.webp",
+        #     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgHMKS3ryrpSrZvnHWfpiyBEmnF1Oy1Tw3IQ&s",
+        #     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGchDhH0FLs53qrVu_VDFAQ80tM-H_Bq2EBg&s",
+        #     "https://media.zenfs.com/en/nownews.com/b34d3759f2b1382430bfa18cac22ced9",
+        #     "https://dailyview.tw/_next/image?url=https%3A%2F%2Fdvblobcdnjp.azureedge.net%2FContent%2FUpload%2FPopular%2FImages%2F2019-02%2F9265b389-0aca-4318-9f2f-226397be36e9_m.jpg&w=1920&q=75",
+        #     "https://img.ltn.com.tw/Upload/ent/page/800/2023/10/27/phpa06t7k.jpg",
+        #     "https://attach.setn.com/newsimages/2020/09/04/2755061-PH.jpg",
+        #     "https://cbu01.alicdn.com/img/ibank/O1CN01TEZ0qm1pCtlUIyYSx_!!3838865325-0-cib.jpg",
+        #     "https://images.chinatimes.com/newsphoto/2019-12-20/656/20191220000037.jpg",
+        #     "https://i.imgur.com/iq67SPm.jpg",
+        #     "https://cdn.hk01.com/di/media/images/dw/20210707/489482591276109824637408.jpeg/sOkuWkg6jdkim0jZYtkbEChKJv3A3Kpw3Pjr2dz469k?v=w1920",
+        #     "https://tshop.r10s.com/b27/50c/cfec/baef/3073/d808/4e76/1145ed930c0242ac110005.jpg?_ex=486x486",
+        #     "https://media.nownews.com/nn_media/thumbnail/2022/03/1647937976105-2ed3e95d6cad43238ed07dcc941aff2b-500x625.jpg?unShow=false&waterMark=false",
+        #     "https://s.yimg.com/zp/images/9DA66C9F780B61728BCB58F044CF4AF6F6ABBCA4",
+        #     "https://img.cdn.91app.hk/webapi/imagesV3/Cropped/SalePage/361662/0/638616321323030000?v=1",
+        #     "https://diz36nn4q02zr.cloudfront.net/webapi/imagesV3/Cropped/SalePage/8085430/0/638580602488200000?v=1",
+        #     "https://tshop.r10s.com/627/448/7fce/3c59/4041/e841/dfd1/116fed83c30242ac110003.jpg?_ex=486x486",
+        #     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTp3dixUyncHWWwFb93xc3ap0AMfwFEdxuLYQ&s",
+        #     "https://down-tw.img.susercontent.com/file/222b1cddcc028fca6be43a23c469121d",
+        #     "https://tshop.r10s.com/9d1/dea/5403/d6d2/308b/3e64/7ca6/11f5eeb5570242ac110002.jpg?_ex=486x486",
+        #     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOu8MbM1UM9f2qoBFArh01bLef3L2AcAbhVw&s",
+        #     "https://img.ltn.com.tw/Upload/ent/page/800/2021/04/22/phpqElaYW.jpg",
+        #     "https://images.chinatimes.com/newsphoto/2018-06-23/656/20180623002206.jpg",
+        #     "https://im2.book.com.tw/image/getImage?i=https://www.books.com.tw/img/001/081/57/0010815753.jpg&v=5c7666aek&w=375&h=375",
+        #     "https://img.ltn.com.tw/Upload/ent/page/800/2020/03/25/phpFHoymZ.jpg",
+        #     "https://cdn.hk01.com/di/media/images/dw/20240531/873078137703895040261407.jpeg/NNjtWk7r1hvScy8dyFMWXQJIMf8k79cvIsXtPiLF7T4?v=w1920",
+        #     "https://bee-men.com/uploads/ckeditor/pictures/25032/content_wendy__624_209358671_322462809344373_4100224133141901654_n.jpg",
+        #     "https://fs2.my-bras.com/upload/ftp/00_Product/22037000W-207-1.jpg",
+        #     "https://image.hkhl.hk/f/1024p0/0x0/100/none/ce50202910d349d7f7ce14913be6456f/2023-06/_alyciac_343289471_538879431656306_1742365294561665946_n.jpg",
+        #     "https://images.chinatimes.com/newsphoto/2020-07-08/656/20200708005597.jpg",
+        #     "https://thumbs.dreamstime.com/z/%E6%BC%82%E4%BA%AE%E7%9A%84%E8%82%96%E5%83%8F%EF%BC%8C%E6%80%A7%E6%84%9F%E7%9A%84%E4%BA%9A%E6%B4%B2%E5%A5%B3%E4%BA%BA%EF%BC%8C%E7%9C%8B%E8%B5%B7%E6%9D%A5%E6%80%A7%E6%84%9F%E8%BF%B7%E4%BA%BA-245021896.jpg",
+        #     "https://diz36nn4q02zr.cloudfront.net/webapi/imagesV3/Original/SalePage/9501574/0/638503534754030000?v=1",
+        #     "https://hk.on.cc/cnt/entertainment/20230625/photo/bkn-20230625210127720-0625_00862_001_02p.jpg?20230625210127",
+        #     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzolZ_BFSbZdm7m-CN2Ij8KikM4Rt7M9hafQ&s",
+        #     "https://images.chinatimes.com/newsphoto/2023-09-15/1024/20230915003039_20230915144323.jpg",
+        #     "https://s.yimg.com/ny/api/res/1.2/Ah4NjV.AOKa.b3tVnQ5Mvg--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MA--/https://media.zenfs.com/zh-tw/nownews.hk/61f741eb6c0da77dcd1b78729907cf3f",
+        #     "https://www.cangai.tw/wp-content/uploads/2021/06/01-3.jpg",
+        #     "https://www.etonwedding.com/pre-wedding-photo/img/%E5%80%8B%E4%BA%BA%E5%AF%AB%E7%9C%9F_%E9%96%A8%E8%9C%9C%E5%AF%AB%E7%9C%9F/6-%E5%80%8B%E4%BA%BA%E5%AF%AB%E7%9C%9F_%E9%96%A8%E8%9C%9C%E5%AF%AB%E7%9C%9F%20(6).jpg",
+        #     "https://s.yimg.com/ny/api/res/1.2/qJHMvzmdgYYx5hIypNXnsw--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MDtoPTgwMA--/https://media.zenfs.com/en/ftvn.com.tw/e5132eb34101fffdae201a88d6c30f49",
+        #     "https://today-obs.line-scdn.net/0hyK1CZDb3JmQMOjIUBE9ZMzRsKhU_XDxtLlxpASg4KAcpFjIwNFp1BykycEhxAjU3LFs9VX1ofV0gX2lgNA/w644",
+        #     "https://img.ltn.com.tw/Upload/news/600/2019/05/29/phpb85WtL.jpg",
+        #     "https://atctwn.com/wp-content/uploads/2021/07/main-4.jpg",
+        #     "https://atctwn.com/wp-content/uploads/2021/07/WNM.jpg",
+        #     "https://cdn2.ettoday.net/images/7803/d7803868.jpg",
+        #     "https://cdn2.ettoday.net/images/4243/d4243363.jpg",
+        #     "https://i1.kknews.cc/OZLWpFd7Sdd6e5Cj0RPSHbH-oLsCiEs_iyR54Sk/0.jpg",
+        #     "https://www.etonwedding.com/img/photo/personal/music/personal%20(1).jpg",
+        #     "https://i.getjetso.com/month_1612/20161209_28719cd2b8a32cd51239FeUOK7.jpg?name=%E6%96%B9%E7%B7%A9%E5%AF%AB%E7%9C%9F",
+        #     "https://cdn2.ettoday.net/images/6862/d6862321.jpg",
+        #     "https://static.wixstatic.com/media/931713_32eb7c8072ef49fb9628c18caeb9e143~mv2.jpg/v1/fill/w_250,h_375,al_c,q_90,enc_auto/931713_32eb7c8072ef49fb9628c18caeb9e143~mv2.jpg",
+        #     "https://img.ltn.com.tw/Upload/ent/page/800/2024/07/08/phpk9uyxg.jpg",
+        #     "https://atctwn.com/wp-content/uploads/2020/02/yoda-1.jpg",
+        #     "https://static.wixstatic.com/media/931713_d0f31e419e9f4099b00ac74dbc9190f6~mv2.jpg/v1/fill/w_250,h_375,al_c,q_90,enc_auto/931713_d0f31e419e9f4099b00ac74dbc9190f6~mv2.jpg",
+        #     "https://i2.kknews.cc/tNTbLd-nwHY_0AvlNEFZWF-wVJJztVi9Ap6WzLg/0.jpg",
+        #     "https://i.pinimg.com/1200x/96/c6/23/96c623821b53e8eb00e12ddabef911dd.jpg"
+        # ]
             
         # 進行圖片URL檢查
         while attempts < max_attempts:
-            # 隨機選擇一個圖片 URL
-            random_image_url = random.choice(image_urls)
+            random_number = random.randint(1, 60)
+            image_url = f"https://raw.githubusercontent.com/hal-chena/Line-Image/refs/heads/main/BigBusted/bigbusted%20({random_number_image_urls_2}).jpg"
+            # # 隨機選擇一個圖片 URL
+            # image_url = random.choice(image_urls)
             
             # 檢查圖片是否存在
-            if check_image_url_exists(random_image_url):
+            if check_image_url_exists(image_url):
                 # 如果圖片存在，回傳訊息
                 line_bot_api.reply_message(
                     event.reply_token,
                     [
-                        ImageSendMessage(original_content_url=random_image_url, preview_image_url=random_image_url)
+                        ImageSendMessage(original_content_url=image_url, preview_image_url=image_url)
                     ]
                 )
                 break  # 找到圖片後退出迴圈
