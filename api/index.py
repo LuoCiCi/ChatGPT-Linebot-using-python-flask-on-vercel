0900f46a7389b7c0b25ca84a3129b963589b966c
Allen <allen@hacontrols.com.tw>
2024-10-14 17:12:15 +0800

    超稀有

--- api/index.py
+++ api/index.py
@@ -924,7 +924,7 @@ def handle_message(event):
                     line_bot_api.reply_message(
                         event.reply_token,
                         [
-                            TextSendMessage(f"恭喜抽中SSR彩蛋編號: {random_number_image_urls_3}"),
+                            TextSendMessage(f"恭喜抽中超稀有SSR彩蛋編號: {random_number_image_urls_3}"),
                             ImageSendMessage(original_content_url=image_urls_3, preview_image_url=image_urls_3)
                         ]
                     )
