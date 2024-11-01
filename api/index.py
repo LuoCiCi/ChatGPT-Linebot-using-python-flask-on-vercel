f94458030b1a569c02f5d352e850c41772ff13a1
Allen <benchen0158@hotmail.com.tw>
2024-11-01 16:23:43 +0800

    修正抽抽抽臭蟲

--- api/index.py
+++ api/index.py
@@ -1101,27 +1101,26 @@ def handle_message(event):
         
         # 進行圖片URL檢查
         while attempts < max_attempts:
-            random_number1 = random.randint(1, 500)
-            random_number2 = random.randint(1, 500)
-            random_number3 = random.randint(1, 500)
-            if (random_number1 != random_number2) and (random_number2 != random_number3) and (random_number3 != random_number1):
-                image_url1 = f"https://raw.githubusercontent.com/hal-chena/Line-Image/refs/heads/main/MoneyMoney/LINE_ALBUM_money_%20({random_number1}).jpg"
-                image_url2 = f"https://raw.githubusercontent.com/hal-chena/Line-Image/refs/heads/main/MoneyMoney/LINE_ALBUM_money_%20({random_number2}).jpg"
-                image_url3 = f"https://raw.githubusercontent.com/hal-chena/Line-Image/refs/heads/main/MoneyMoney/LINE_ALBUM_money_%20({random_number3}).jpg"
-                
-                # 檢查圖片是否存在
-                if check_image_url_exists(image_url1) and check_image_url_exists(image_url2) and check_image_url_exists(image_url3):
-                    # 如果圖片存在，回傳訊息
-                    line_bot_api.reply_message(
-                        event.reply_token,
-                        [
-                            ImageSendMessage(original_content_url=image_url1, preview_image_url=image_url1),
-                            ImageSendMessage(original_content_url=image_url2, preview_image_url=image_url2),
-                            ImageSendMessage(original_content_url=image_url3, preview_image_url=image_url3)
-                        ]
-                    )
-                    break  # 找到圖片後退出迴圈
-                attempts += 1
+            random_numbers = random.sample(range(1, 501), 3)
+            random_number1, random_number2, random_number3 = random_numbers
+            
+            image_url1 = f"https://raw.githubusercontent.com/hal-chena/Line-Image/refs/heads/main/MoneyMoney/LINE_ALBUM_money_%20({random_number1}).jpg"
+            image_url2 = f"https://raw.githubusercontent.com/hal-chena/Line-Image/refs/heads/main/MoneyMoney/LINE_ALBUM_money_%20({random_number2}).jpg"
+            image_url3 = f"https://raw.githubusercontent.com/hal-chena/Line-Image/refs/heads/main/MoneyMoney/LINE_ALBUM_money_%20({random_number3}).jpg"
+            
+            # 檢查圖片是否存在
+            if check_image_url_exists(image_url1) and check_image_url_exists(image_url2) and check_image_url_exists(image_url3):
+                # 如果圖片存在，回傳訊息
+                line_bot_api.reply_message(
+                    event.reply_token,
+                    [
+                        ImageSendMessage(original_content_url=image_url1, preview_image_url=image_url1),
+                        ImageSendMessage(original_content_url=image_url2, preview_image_url=image_url2),
+                        ImageSendMessage(original_content_url=image_url3, preview_image_url=image_url3)
+                    ]
+                )
+                break  # 找到圖片後退出迴圈
+            attempts += 1
         else:
             # 如果在max_attempts次內未找到有效圖片
             line_bot_api.reply_message(
