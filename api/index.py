76124e420951ce6b5cf12686c8b30de7f3628a16
Allen <benchen0158@hotmail.com.tw>
2024-11-15 11:25:27 +0800

    bug1

--- api/index.py
+++ api/index.py
@@ -1006,7 +1006,6 @@ def handle_message(event):
             line_bot_api.reply_message(
                 event.reply_token,
                 [
-                    TextSendMessage(text=response_text),
                     ImageSendMessage(original_content_url=image_url, preview_image_url=image_url)
                 ]
             )
