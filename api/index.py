d3dd222e52326e8e19ab660c3f72e4a9ac683073
Allen <benchen0158@hotmail.com.tw>
2024-11-18 11:03:49 +0800

    彩蛋機率降低

--- api/index.py
+++ api/index.py
@@ -1062,7 +1062,7 @@ def handle_message(event):
         while attempts < max_attempts:    
             
             random_value = random.random()        
-            if random_value < 0.1:  # 10% 機率
+            if random_value < 0.05:  # 10% 機率
                 
                 random_number_image_urls_2 = random.randint(1,72)
                 image_urls_2 = f"https://raw.githubusercontent.com/hal-chena/Line-Image/refs/heads/main/Drawing/SR%20({random_number_image_urls_2}).jpg"
@@ -1078,7 +1078,7 @@ def handle_message(event):
                         ]
                     )
                     break  # 找到圖片後退出迴圈
-            elif random_value < 0.1 + 0.03: #3% 機率
+            elif random_value < 0.05 + 0.02: #3% 機率
                 
                 random_number_image_urls_3 = random.randint(1,29)
                 image_urls_3 = f"https://raw.githubusercontent.com/hal-chena/Line-Image/refs/heads/main/Drawing/SSR%20({random_number_image_urls_3}).jpg"
