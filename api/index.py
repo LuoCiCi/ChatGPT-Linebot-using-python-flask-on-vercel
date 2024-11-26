f29edfb88020ad0202e25f833391c7c05ea88cc9
LuoCiCi <125720586+LuoCiCi@users.noreply.github.com>
2024-11-26 16:00:44 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1066,7 +1066,7 @@ def handle_message(event):
             random_value = random.random()        
             if random_value < 0.05:  # 10% 機率
                 
-                random_number_image_urls_2 = random.randint(1,72)
+                random_number_image_urls_2 = random.randint(1,77)
                 image_urls_2 = f"https://raw.githubusercontent.com/hal-chena/Line-Image/refs/heads/main/Drawing/SR%20({random_number_image_urls_2}).jpg"
                 
                 if check_image_url_exists(image_urls_2):
@@ -1082,7 +1082,7 @@ def handle_message(event):
                     break  # 找到圖片後退出迴圈
             elif random_value < 0.05 + 0.02: #3% 機率
                 
-                random_number_image_urls_3 = random.randint(1,29)
+                random_number_image_urls_3 = random.randint(1,30)
                 image_urls_3 = f"https://raw.githubusercontent.com/hal-chena/Line-Image/refs/heads/main/Drawing/SSR%20({random_number_image_urls_3}).jpg"
                 
                 if check_image_url_exists(image_urls_3):
