d637a101cda8b4d632c5c65a318d5db72c50f3cc
Allen <benchen0158@hotmail.com.tw>
2024-12-02 13:52:24 +0800

    更新抽美食編號不對稱問題

--- api/index.py
+++ api/index.py
@@ -1058,10 +1058,13 @@ def handle_message(event):
                 TextSendMessage(text="抓取美食json失敗，真是個廢物")
             )
             return           
-        random_id = random.randint(1, 2)
-        image_url = f"https://raw.githubusercontent.com/hal-chena/Line-Image/refs/heads/main/Food/Food%20({random_id}).jpg"
+        # 隨機選擇編號
+        random_id = random.randint(1, len(Food_data))
+        random_id_str = f"{random_id:03d}"
+        image_url = f"https://raw.githubusercontent.com/hal-chena/Line-Image/main/Food/Food%20({random_id}).jpg"
+        
         # 查找對應編號的美食資料
-        Food = next((p for p in Food_data if p['編號'] == f"#{random_id:03d}"), None)
+        Food = next((p for p in Food_data if p['編號'] == random_id_str), None)
         if Food and check_image_url_exists(image_url):
             # 組合回應訊息
             response_text = f"美食圖鑑編號: {Food['編號']}\n"
