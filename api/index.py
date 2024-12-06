7f0c77fb24ca77742e2b8d5a068d109db809139c
LuoCiCi <125720586+LuoCiCi@users.noreply.github.com>
2024-12-06 18:33:57 +0800

    Update bug

--- api/index.py
+++ api/index.py
@@ -1146,6 +1146,8 @@ def handle_message(event):
         return
     
     if event.message.text == "抽":
+        global limit
+
         working_status = False
         max_attempts = 5  # 設定最多嘗試的次數
         attempts = 0        
