d1e658f6cf4e352feaeddfb6dd0743c28a79aca8
LuoCiCi <125720586+LuoCiCi@users.noreply.github.com>
2024-12-06 17:21:41 +0800

    groupid bug

--- api/index.py
+++ api/index.py
@@ -355,15 +355,15 @@ def handle_message(event):
     # line_bot_api.push_message("U86fd4e0cce57a1b2d5ec119c8f9d6d7e", TextSendMessage(text=f"user_id:{user_id}"))
     
     # 檢查是否為群組訊息
-    if event.source.type == 'group':
-        # 取得群組 ID
-        group_id = event.source.group_id
-        #line_bot_api.push_message(user_id, TextSendMessage(text='TEST'))
-        # 回傳群組 ID 作為測試
-        line_bot_api.reply_message(
-            "U86fd4e0cce57a1b2d5ec119c8f9d6d7e",
-            TextSendMessage(text=f"群組 ID 是: {group_id}")
-        )
+    # if event.source.type == 'group':
+    #     # 取得群組 ID
+    #     group_id = event.source.group_id
+    #     #line_bot_api.push_message(user_id, TextSendMessage(text='TEST'))
+    #     # 回傳群組 ID 作為測試
+    #     line_bot_api.reply_message(
+    #         "U86fd4e0cce57a1b2d5ec119c8f9d6d7e",
+    #         TextSendMessage(text=f"群組 ID 是: {group_id}")
+    #     )
     
         # return
     
