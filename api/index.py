e456000ec93fb2a5c7bcb8d0e1573efc4448a994
LuoCiCi <125720586+LuoCiCi@users.noreply.github.com>
2025-11-13 11:48:53 +0800

    debug

--- api/index.py
+++ api/index.py
@@ -748,7 +748,7 @@ def handle_message(event):
         
         line_bot_api.reply_message(event.reply_token,
                 [
-                    TextSendMessage(f"{text_message} 預報資料")
+                    TextSendMessage(f"{text_message_decoded} 預報資料")
                 ]) # 傳送文字
         
 
