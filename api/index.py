29d9c462f1e3a1970eed615e27b59387f8ded528
LuoCiCi <125720586+LuoCiCi@users.noreply.github.com>
2025-11-13 14:13:34 +0800

    test future weather

--- api/index.py
+++ api/index.py
@@ -744,7 +744,8 @@ def handle_message(event):
 
 
          # 中央氣象署未來天氣預報 API（全台縣市）
-         url = "https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-D0047-001"
+        
+         url = f'https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-D0047-001'
          params = {
              "Authorization": "CWA-84D9233C-12BC-4CD7-B744-7C7F35F7AE48",
              "format": "JSON"
