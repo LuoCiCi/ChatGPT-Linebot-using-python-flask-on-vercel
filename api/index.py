65167d26416ff67c18a3e69c8b31648ed185177e
Allen <benchen0158@hotmail.com.tw>
2025-11-14 14:09:11 +0800

    不炸毛了

--- api/index.py
+++ api/index.py
@@ -2429,7 +2429,7 @@ def handle_message(event):
         if not data:
             line_bot_api.reply_message(
                 event.reply_token,
-                TextSendMessage(text=f"查無股票代號 {stock_id} 或資料異常")
+                TextSendMessage(text=f"查無股票代號 {stock_id} 或是你呆呆寄錯號碼")
             )
             return
         
